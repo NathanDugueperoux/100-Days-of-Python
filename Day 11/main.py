76422fdaba942479a4702643202d83b886079845
nathan <nathandugueperoux554@gmail.com>
2024-05-03 17:23:41 +0100

    Day 11

--- Day 11/main.py
+++ Day 11/main.py
@@ -4,53 +4,76 @@ cards = [11, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 dealer = []
 player = []
 
+
+def give_initial_decks():
+    for i in range(2):
+        player.append(random.choice(cards))
+        dealer.append(random.choice(cards))
+
+
+def calculate_score(deck: list):
+    score = 0
+    for i in deck:
+        score += i
+    return score
+
+
+def check_win(p: list, d: list):
+    p_score = calculate_score(p)
+    d_score = calculate_score(d)
+    if p_score > 21 and d_score > 21:
+        print("You drew, both of you decks have a sum greater than 21.")
+    elif p_score > 21:
+        print("You lose, your cards have a sum greater than 21.")
+    elif d_score > 21:
+        print("You win, the dealers cards have a sum that exceeds 21.")
+    elif d_score > p_score:
+        print("You lose, the dealers cards have a sum greater than yours.")
+    else:
+        print("You win, your cards have a sum greater than the dealers.")
+
+
+def check_ace(deck: list):
+    for i in range(len(deck)):
+        if deck[i] == 11:
+            deck[i] = 1
+            if calculate_score(deck) < 21:
+                break
+
+
+def check_dealer():
+    if calculate_score(dealer) < 17:
+        dealer.append(random.choice(cards))
+        if calculate_score(dealer) > 21:
+            check_ace(dealer)
+
+
 while True:
     start = input("Do you want to play a game of Blackjack?: ").lower()
-    if start == "no" or "n":
+    if start == "no" or start == "n":
         break
-    elif start == "yes" or "y":
-        for i in range(2):
+    elif start == "yes" or start == "y":
+        give_initial_decks()
+        print(f"Dealer: [{dealer[0]}, ##]\nTotal = {dealer[0]}\n\nPlayer: [{player[0]}, {player[1]}]\nTotal = {calculate_score(player)}")
+        draw_card = input("Do you want to draw another card?: ").lower()
+        if draw_card == "no" or draw_card == "n":
+            check_dealer()
+            if calculate_score(player) > 21:
+                check_ace(player)
+            print(f"Dealer: {dealer}\nTotal = {calculate_score(dealer)}\n\nPlayer: [{player[0]}, {player[1]}]\nTotal = {calculate_score(player)}")
+            check_win(player, dealer)
+            player.clear()
+            dealer.clear()
+        elif draw_card == "yes" or draw_card == "y":
             player.append(random.choice(cards))
-            dealer.append(random.choice(cards))
+            if calculate_score(player) > 21:
+                check_ace(player)
+            if calculate_score(dealer) > 21:
+                check_ace(dealer)
+            print(f"Dealer: {dealer}\nTotal = {calculate_score(dealer)}\n\nPlayer: [{player}]\nTotal = {calculate_score(player)}")
+            check_win()
 
-        if player[0] == 11 and player[1] == 11:
-            player[0] = 1
-        if dealer[0] == 11 and dealer[1] == 11:
-            dealer[0] = 1
 
-        print(f"\nDealers hand: [{dealer[0]}, ##]\nTotal = {dealer[0]}\n\nPlayers hand: [{player[0]}, {player[1]}]\nTotal = {player[0]+player[1]}\n")
 
-        take_card = input("Do you wish to take another card?:").lower()
-        if take_card == "no" or take_card == "n":
-            if (dealer[0]+dealer[1]) > 17:
-                print("Since dealers sum of cards is less than 17, Dealer must take another card.")
-                dealer.append(random.choice(cards))
-                if (dealer[0]+dealer[1]+dealer[2]) > 21 and 11 in dealer:
-                    while True:
-                        for i in range(len(dealer)):
-                            if dealer[i] == 11:
-                                dealer[i] = 1
-                                if (dealer[0]+dealer[1]+dealer[2]) < 21:
-                                    break
-                                elif 11 not in dealer:
-                                    break
-                                else:
-                                    pass
-                if ((dealer[0]+dealer[1]+dealer[2]) > 21 and 11 not in dealer) and (player[0]+player[1]) < 21:
-                    print(f"\nDealers hand: [{dealer[0]}, {dealer[1]}, {dealer[2]}]\nTotal = {dealer[0]+dealer[1]+dealer[2]}\n\nPlayers hand: [{player[0]}, {player[1]}]\nTotal = {player[0]+player[1]}\n")
-                    print("You win, Dealers sum exceeds 21")
-                elif ((dealer[0] + dealer[1] + dealer[2]) > 21 and 11 not in dealer) and (player[0] + player[1]) > 21:
-                    print(f"\nDealers hand: [{dealer[0]}, {dealer[1]}, {dealer[2]}]\nTotal = {dealer[0]+dealer[1]+dealer[2]}\n\nPlayers hand: [{player[0]}, {player[1]}]\nTotal = {player[0] + player[1]}\n")
-                    print("You drew, both your sums exceed 21")
-                elif ((dealer[0] + dealer[1] + dealer[2]) < 21) and (player[0] + player[1]) > 21:
-                    print(f"\nDealers hand: [{dealer[0]}, {dealer[1]}, {dealer[2]}]\nTotal = {dealer[0] + dealer[1] + dealer[2]}\n\nPlayers hand: [{player[0]}, {player[1]}]\nTotal = {player[0] + player[1]}\n")
-                    print("You lost, your sum exceeds 21")
-                elif ((dealer[0] + dealer[1] + dealer[2]) < 21) and (player[0] + player[1]) < 21:
-                    if (dealer[0] + dealer[1] + dealer[2]) > (player[0] + player[1]):
-                        print(f"\nDealers hand: [{dealer[0]}, {dealer[1]}, {dealer[2]}]\nTotal = {dealer[0] + dealer[1] + dealer[2]}\n\nPlayers hand: [{player[0]}, {player[1]}]\nTotal = {player[0] + player[1]}\n")
-                        print("Dealer wins.")
-                    else:
-                        print(f"\nDealers hand: [{dealer[0]}, {dealer[1]}, {dealer[2]}]\nTotal = {dealer[0] + dealer[1] + dealer[2]}\n\nPlayers hand: [{player[0]}, {player[1]}]\nTotal = {player[0] + player[1]}\n")
-                        print("Player wins.")
-            else:
-                pass
+
+
